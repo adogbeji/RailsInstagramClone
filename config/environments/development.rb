07ba3a4879b12b2fa13956e8f260d9500a3ae255
adogbeji <baka_1993@yahoo.co.uk>
2023-05-28 18:11:33 +0000

    Whitelist AWS address

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,4 +67,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  
+  # config.hosts = nil
+  config.hosts << "1ac97c8b5e704476b7fb7f6e8f0e9cfa.vfs.cloud9.eu-west-2.amazonaws.com"
 end
